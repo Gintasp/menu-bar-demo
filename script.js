28e0843bb052d9fc2045f83ac6c614e1af2d4613
Gintasp <gplonis@gmail.com>
2018-05-02 09:31:13 +0300

    Clean js

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ var menuTab=document.querySelectorAll("li");
 var burger=document.querySelector(".burger");
 var menu=document.querySelector("ul");
 
+function shrink(){
+	return $("ul").removeClass("flex-vertical").addClass("flex");
+}
+function expand(){
+	return $("ul").removeClass("flex").addClass("flex-vertical");
+}
+
 for(var i=0;i<menuTab.length;i++){
 	menuTab[i].addEventListener("click",function(){
 		if(!this.classList.contains("active")){
@@ -10,12 +17,6 @@ for(var i=0;i<menuTab.length;i++){
 		}
 	});
 };
-function shrink(){
-	return $("ul").removeClass("flex-vertical").addClass("flex");
-}
-function expand(){
-	return $("ul").removeClass("flex").addClass("flex-vertical");
-}
 
 $(".burger").on("click",function(){
 	$(".burger").html("<i class='fas fa-times'></i>");
