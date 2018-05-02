5361f58f51ec17d548e16a0b92d0bea76abde5ba
Gintasp <gplonis@gmail.com>
2018-05-02 09:27:32 +0300

    Add js

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 var menuTab=document.querySelectorAll("li");
 var burger=document.querySelector(".burger");
 var menu=document.querySelector("ul");
+
+for(var i=0;i<menuTab.length;i++){
+	menuTab[i].addEventListener("click",function(){
+		if(!this.classList.contains("active")){
+			this.classList.add("active");
+			$(this).siblings().removeClass("active");
+		}
+	});
+};
+function shrink(){
+	return $("ul").removeClass("flex-vertical").addClass("flex");
+}
+function expand(){
+	return $("ul").removeClass("flex").addClass("flex-vertical");
+}
+
+$(".burger").on("click",function(){
+	$(".burger").html("<i class='fas fa-times'></i>");
+	if($("ul").hasClass("flex-vertical")){
+		$(".burger").html("<i class='fas fa-bars'></i>");
+		shrink();
+	}else{
+		expand();
+	}
+});
